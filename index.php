e8e51dd8d30eaeaf03b0717f406390f0a5de962c
Aliia02 <80500210+Aliia02@users.noreply.github.com>
2021-03-19 10:15:26 +0600

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="main.php" method="get">
+Login: <input type="text" name="name"><br>
+Password: <input type="password" name="123"><br>
+<button type="button">CLEAR</button>
+<button type="button">SUBMIT</button>
+
+
+
+</form>
+
+</body>
+</html>
